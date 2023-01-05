fe1220466e400d2019f31109b81b5daa2eb82ba4
HengweiXu <1761173100@qq.com>
2023-01-05 14:06:09 +0800

    后端添加post部分

--- controller/post.go
+++ controller/post.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"goblog-end/dao"
 	"goblog-end/model"
 
@@ -15,6 +16,19 @@ func Post(c *gin.Context) {
 
 	//查询数据库
 	dataList = dao.GetAllPost()
+	for i := 0; i < len(dataList); i++ {
+		if len(dataList[i].Content) > 100 {
+			dataList[i].Content = dataList[i].Content[0:100] + "..."
+		}
+		fmt.Printf("v: %v\n", dataList[i].Content)
+
+	}
+	// for _, v := range dataList {
+	// 	if len(v.Content) > 100 {
+	// 		v.Content = v.Content[:100]
+	// 	}
+	// 	fmt.Printf("v: %v\n", v.Content)
+	// }
 
 	//判断是否查到数据
 	if len(dataList) == 0 {

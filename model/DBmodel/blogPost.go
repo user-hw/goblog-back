b873a08b74c995de912865d9481653e41e06c505
HengweiXu <1761173100@qq.com>
2023-02-03 00:28:29 +0800

    修改部分数据

--- model/DBmodel/blogPost.go
+++ model/DBmodel/blogPost.go
@@ -2,7 +2,7 @@
  * @Author: HengweiXu 1761173100@qq.com
  * @Date: 2023-01-04 16:32:27
  * @LastEditors: HengweiXu 1761173100@qq.com
- * @LastEditTime: 2023-01-31 20:55:06
+ * @LastEditTime: 2023-02-02 23:33:35
  * @FilePath: /goblog-back/model/blogPost.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,10 +20,15 @@ type BlogPost struct {
 	Title        string `json:"title"`        // 文章标题
 	Content      string `json:"content"`      // 文章的html
 	Text         string `json:"text"`         // 文章的文字
+	TitleImg     string `json:"titleImg"`     // 封面图片
+	ShortText    string `json:"shortText"`    // 简介文字，留着备用
 	CategoryId   int    `json:"categoryId"`   // 分类id
 	Uid          int    `json:"uid "`         // 用户id
 	ViewCount    int    `json:"viewCount"`    // 查看次数
 	CommentCount int    `json:"commentCount"` // 评论次数
+	LikeCount    int    `json:"likeCount"`    // 点赞次数
+	SubCount     int    `json:"subCount"`     // 订阅次数
+	MetaUrl      string `json:"metaUrl"`      //原文链接
 	Type         int    `json:"type"`         //文章类型 0 普通，1 自定义文章
 }
 
@@ -38,6 +43,8 @@ type BlogPostA struct {
 	Uid          int    `json:"Uid "`         // 用户id
 	ViewCount    int    `json:"viewCount"`    // 查看次数
 	CommentCount int    `json:"commentCount"` // 评论次数
+	LikeCount    int    `json:"likeCount"`    // 点赞次数
+	SubCount     int    `json:"subCount"`     // 订阅次数
 	Type         int    `json:"type"`         //文章类型 0 普通，1 自定义文章
 	// CreateAt     time.Time `json:"createAt"`     // 创建时间
 	// UpdateAt     time.Time `json:"updateAt"`     // 更新时间
